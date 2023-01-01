4d9e55400351c95ca46bce3b09b973dfc97af9d6
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-01 22:16:41 +0300

    Time: 60 ms (94.41%), Space: 15.2 MB (24.50%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+   def twoSum(self, nums, target):
+    seen = {}
+    for i, v in enumerate(nums):
+        remaining = target - v
+        if remaining in seen:
+            return [seen[remaining], i]
+        seen[v] = i
+    return []
