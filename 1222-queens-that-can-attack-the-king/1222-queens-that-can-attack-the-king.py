623be89078275e675899606016984f120f75409f
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-10 22:55:23 +0300

    Time: 42 ms (88.40%), Space: 13.8 MB (89.34%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,15 +1,12 @@
 class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
-        ans = []
-        
-        queens = {(i, j) for i, j in queens}
-        
-        for i in [-1, 0, 1]:
-           
-            for j in [-1, 0, 1]:
-                for k in range(1, 8):
-                    x, y = king[0] + i * k, king[1] + j * k
-                    if (x, y) in queens:
-                        ans.append([x, y])
+        ans=[]
+        for horizontal in [-1,0,1]:
+            for vertical in [-1,0,1]:
+                for distance in range(1,8):
+                    x=king[0]+horizontal*distance
+                    y=king[1]+vertical*distance
+                    if [x,y] in queens:
+                        ans.append([x,y])
                         break
         return ans
