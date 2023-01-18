cf86afbb87e68da54076888b7eddb6335badc24a
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-18 20:51:42 +0300

    Time: 82 ms (99.79%), Space: 13.8 MB (79.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,10 +1,43 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # this is user friendly solution for this problem
+        for i in range(9):
+            seen = set()
+            for j in range(9):
+                curr = board[i][j]
+                if curr == '.':
+                    continue
+                if curr in seen:
+                    return False
+                seen.add(curr)
+        for j in range(9):
+            seen = set()
+            for i in range(9):
+                curr = board[i][j]
+                if curr == '.':
+                    continue
+                if curr in seen:
+                    return False
+                seen.add(curr)
+        for i in range(3):
+            for j in range(3):
+                seen = set()
+                for r in range(i*3,i*3+3):
+                    for c in range(j*3,j*3+3):
+                        curr = board[r][c]
+                        if curr == '.':
+                            continue
+                        if curr in seen:
+                            return False
+                        seen.add(curr)
+        return True
+
+# this is the modefied one 
+'''class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
         box = collections.defaultdict(set)
-        print(rows)
-        print(box)
         for row in range(9):  #length of board is == 9
             for col in range(9):
                 if board[row][col]=='.':
@@ -16,4 +49,4 @@ class Solution:
                 rows[row].add(board[row][col])
                 cols[col].add(board[row][col])
                 box[row//3,col//3].add(board[row][col])
-        return True
+        return True'''
