c34b3f33dfb3499b39e2953d13dc11e172e258c2
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-19 21:40:24 +0300

    Time: 748 ms (88.50%), Space: 40.8 MB (39.67%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,9 +1,9 @@
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
         n = len(nums)
-        ans = nums
+        ans = nums                   # =>transfer to other arr
         for i in range(n):
-            string = str(nums[i])
-            reverse = string[::-1]
-            ans.append(int(reverse))
-        return len(set(ans))
+            string = str(nums[i])    # => change int in arr to string 
+            reverse = string[::-1]   # =>reverse string by slice 
+            ans.append(int(reverse)) # =>append reversed array in to new array
+        return len(set(ans))         # =>change new array in to set then return its length 
