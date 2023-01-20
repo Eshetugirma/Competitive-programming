e5aed8be2bd7ebc1b99a261cefc72ba957cf989b
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-20 21:20:38 +0300

    Create Counting_Sort.py

---
+++ Counting_Sort.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    my_dict = {}
+    for j in range(100):
+        my_dict[j] = 0
+    for i in arr:
+        if i in arr:
+            my_dict[i] += 1
+    return my_dict.values()
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
