548c26dbc42826f719d9f8a9fb1c2c8fb460f7e5
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-22 18:46:44 +0300

    Time: 524 ms (26.55%), Space: 15.8 MB (7.86%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,16 +1,18 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # ==> solved with insertion sort
-        for i in range(1,len(heights)):
-            key_name = names[i]
-            key_height = heights[i]
-            right = i-1
-            # ==> this loop is to shift sorted elememts up to see the suitable place for current value
-            while right >= 0 and key_height > heights[right]:
-                heights[right+1] = heights[right]
-                names[right+1] = names[right]
-                right -= 1
-            # ==> put here in sorted part current value 
-            heights[right+1] = key_height
-            names[right+1] = key_name
+        maxima = max(heights)
+        count = [0]*(maxima+1)
+        temp = heights[:]
+        temp2 = names[:]
+        #==> adding their heights to counter
+        for i in heights:
+            count[i] +=1
+        target = 0
+        for i in range(len(count)-1,-1,-1):
+            if count[i]:
+                names[target] = temp2[temp.index(i)]
+                heights[target] = i
+                target += 1
         return names
+                
+        
