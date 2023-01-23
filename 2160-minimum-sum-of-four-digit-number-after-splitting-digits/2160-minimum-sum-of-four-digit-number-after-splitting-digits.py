f1287579c7f5d4d7ee0ac78a01a99525ebc191ed
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-23 08:21:06 +0300

    Time: 41 ms (38.77%), Space: 13.9 MB (51.14%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -2,15 +2,18 @@ class Solution:
     def minimumSum(self, num: int) -> int:
         new1 = []
         new2 = []
+        # ==>> after int is changed to list then strip all right zeros of reverse sorted list
         num = sorted(list(str(num)),reverse=True)
         num = ''.join(num)
         num = list(num.rstrip('0'))
-        if len(num) <= 1:
+        # ==>> if length of num after striped is one then return int in num
+        if len(num) == 1:
             return int(''.join(num))
+        # ==>> this is to append minimum in to both new1 and new2
         for i in range(len(num)):
             if i%2 == 0:
                 new1.append(num.pop())
             else:
                 new2.append(num.pop())
-        minimum = int(''.join(new1)) + int(''.join(new2))
-        return minimum
+        # ==>> return the min sum
+        return int(''.join(new1)) + int(''.join(new2))
