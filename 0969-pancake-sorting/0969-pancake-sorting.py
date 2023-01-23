884ddac0944eb5dbe7a76029d985cf2d59e89d63
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-23 16:51:57 +0300

    Time: 77 ms (30.55%), Space: 13.9 MB (72.06%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,15 +1,20 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        swaps = []
+        ans = []
         target = len(arr)
+        # ==>> loop up to all elements are sorted 
         while target != 0:
             i = arr.index(target)
+            # ==>> if the largest number is get his place then reduce my target and continue
             if i + 1 == target:
                 target -= 1
                 continue
-
-            k = target if i == 0 else i + 1
+            # ==>> if index of target is zero then reverse elements lenght of target else up to position of target
+            if i == 0 :
+                k = target
+            else:
+                k = i + 1
             arr = arr[:k][::-1] + arr[k:]
-            swaps.append(k)
-
-        return swaps
+            # ==>> append lenght of reversed elements
+            ans.append(k)
+        return ans
