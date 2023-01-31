a029379ae19ae6ade1c6591095fbda9720c3778d
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-01-31 07:32:22 +0300

    Time: 38 ms (72.83%), Space: 15.3 MB (99.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head :
@@ -14,4 +15,17 @@ class Solution:
             head.next.next = head
         head.next = None
         return new_head
-
+        '''
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current_head = head
+        prev_head = None
+        while current_head:
+            #==>> iterative mothed updating current head to next and converting head to tail
+            temp = current_head.next
+            current_head.next = prev_head
+            prev_head = current_head
+            current_head = temp
+        return prev_head
+            
+        
