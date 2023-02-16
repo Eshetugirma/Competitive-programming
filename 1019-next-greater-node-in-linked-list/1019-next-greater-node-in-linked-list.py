9c08ee3e17baed3a36f09e20bdb567b13b567f86
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-02-16 21:19:33 +0300

    Time: 337 ms (45.22%), Space: 18.9 MB (58.27%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        curr = head
+        res = []
+        stack = []
+        count = 0
+        #==>>> take every element and check if there is next greater 
+        while head:
+            while stack and stack[-1][1] < head.val:
+                res[stack.pop()[0]] = head.val
+            stack.append([len(res), head.val])
+            res.append(0)
+            head = head.next
+        return res
