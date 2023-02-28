89d0b13b8c80f43c8987350502f46d04a878547d
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-02-28 18:59:45 +0300

    Time: 44 ms (35.43%), Space: 13.9 MB (68.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,23 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        new = ListNode()
-        head = new
-        head1 = list1
-        head2 = list2
-        #==>>>> add every less Node in both list iteratevly
-        while head1 and head2:
-            if head1.val <= head2.val:
-                head.next = head1
-                head1 = head1.next
-                head = head.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode],head=None,dummy=None) -> Optional[ListNode]:
+        #==>> create head node and hold in dummy 
+        if not head:
+            head = ListNode()
+            dummy = head
+        #==>> if both list hold element then add the minimum to head 
+        if list1 and list2:
+            if list1.val <= list2.val:
+                head.next = ListNode(list1.val)
+                return self.mergeTwoLists(list1.next,list2,head.next,dummy)
             else:
-                head.next = head2
-                head2 = head2.next
-                head = head.next
-        if head1:
-            head.next = head1
+                head.next = ListNode(list2.val)
+                return self.mergeTwoLists(list1,list2.next,head.next,dummy)
+        #==>>> when one of list element is empty then add non empity list to head 
+        if list1:
+            head.next = list1
         else:
-            head.next = head2
-        return new.next
+            head.next = list2
+        #==>> now return next of your dummy which is the sorted one 
+        return dummy.next
