cb0961917467eb27ae5af6ffbbacb4a6cdf3b5a5
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-03-03 18:34:27 +0300

    Time: 780 ms (10.52%), Space: 15.6 MB (16.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,11 +3,8 @@ class Solution:
         left, right = 1, max(piles)
         ans = right
         
-        if len(piles) == 1:
-            return piles[0]//h + int(piles[0]%h > 0)
-        
         #==>>> use binary search on max and min of given piles
-        while left <= right:
+        while left < right:
             mid = (left+right)//2            
             time = 0
             
@@ -17,20 +14,14 @@ class Solution:
                 if time > h:
                     break
             
-            #==>> if she finished in given time then take mid and update right
-            if time == h :
-                ans = min(mid,ans)
-                right = mid-1
-            
             #==>> also if she finished before given time update right to mid
-            elif time < h:
-                ans = min(mid,ans)
-                right = mid-1
-            
+            if time <= h:
+                right = mid
+                
             #==>> if time needed is greater than time given move left
             else:
                 left = mid+1
         
-        return ans
+        return right
                 
             
