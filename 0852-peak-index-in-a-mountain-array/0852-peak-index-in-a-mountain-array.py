d14b76d0fe3053f6d1e62d69e4beb67a436303f0
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-03-05 16:49:08 +0300

    Time: 599 ms (87.60%), Space: 27.8 MB (56.14%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,4 +1,14 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        mx = max(arr)
-        return arr.index(mx)
+        left = 0
+        right = len(arr)
+        #==>>> binary search on index where the max up to lenght of arr
+        while right > left:
+            mid = (left+right)//2
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+            if arr[mid] < arr[mid-1]:
+                right = mid 
+            else:
+                left = mid + 1
+        return mid
