3370afb2859291a316d3dbbec8b92856992c0faf
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-03-05 20:49:29 +0300

    Time: 45 ms (46.44%), Space: 13.8 MB (93.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,12 +3,17 @@ class Solution:
         left = 0
         right = x
         mid = 0
+        #==>>> this is to handle the edge case at one 
         if x == 1:
             return 1
+    
         while left < right:
             mid = (left+right)//2
+            #==>> if see the sqrt return and break
             if mid*mid == x or mid*mid < x < (mid+1)*(mid+1):
                 return mid
+            
+            #==>> if mid is greater than expected val then take to right else to left
             if mid * mid > x:
                 right = mid
             else :
