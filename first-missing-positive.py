50436a2e23a0ffab7200ef9961fd5784836468c1
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-03-29 16:14:02 +0300

    added first-missing-positive.python

--- first-missing-positive.py
+++ first-missing-positive.py
@@ -1,16 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        nums.sort()
-        if  nums[0] > 1 or nums[-1] < 1:
-            return 1
-        elif len(nums) == 1:
-            return 2 
-        for i in range(len(nums)-1):
-            if nums[i] <= 0 and nums[i+1]  > 1:
-                return 1
-            if nums[i] <= 0:
-                continue
-            if nums[i]+1 != nums[i+1]:
-                return (nums[i] + 1)
-        return nums[-1]+1
+        dic = defaultdict(int)
+        for i in range(len(nums)):
+            if nums[i] > len(nums) or nums[i] < 0:
+                nums[i] = 0
+            dic[nums[i]] = 1
+        if 0 in dic:
+            del dic[0] 
+        if not dic:
+            return 1 
+        for i in range(len(nums)+1):
+            if i+1 not in dic:
+                return i+1
