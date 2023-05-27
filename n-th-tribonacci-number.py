a834f967eebf95c0f170813cfcd31c859cb140c6
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-05-27 23:07:12 +0300

    added n-th-tribonacci-number.python

---
+++ n-th-tribonacci-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        arr = [0,1,1]
+        for i in range(n-2):
+            arr.append(arr[i]+arr[i+1]+arr[i+2])
+        return arr[n]
