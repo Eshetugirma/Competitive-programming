96b91060b3a14494a4456d09ffa0d8acf032ce80
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-09-19 10:30:55 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -6,8 +6,8 @@ class Solution:
             while not n%start :
                 ans += start
                 n //= start
-            if start == 2:
-                start += 1
+            if start == 2: 
+                start += 1 
             else:
                 start += 2
         if n != 1:
