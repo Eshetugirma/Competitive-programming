e83bbadef036c65bf440395de0b65ad4807127c4
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-10-04 15:02:54 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,24 +1,16 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
 
-        connection = defaultdict(list)
-        #===>>>> here we can simply construct graph or generate connections
-        for i in range(len(prerequisites)):
-            connection[prerequisites[i][1]].append(prerequisites[i][0])
-        #==>>> this is bfs function to find if there is such prerequisite in connection
-        def bfs(que,u,v):
-            que.append(v)
-            visited = set()
-            while que:
-                v = que.popleft()
-                if v == u: return True
-                for curr in connection[v]:
-                    if curr not in visited:
-                        que.append(curr)
-                        visited.add(curr)
-            return False
-        #==>>> this it to check and record every given queries
+        grid = [[False]*numCourses for i in range(numCourses)]
+        for i,j in prerequisites:
+            grid[i][j] = True
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    grid[i][j] = grid[i][j] or (grid[i][k] and grid[k][j])
         ans = []
-        for u,v in queries:
-            ans.append(bfs(deque(),u,v))
+        # print(grid)
+        for i,j in queries:
+            ans.append(grid[i][j])
         return ans
