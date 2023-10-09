3f60ba6745927a92807a3bda4bb196e249490a68
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-10-09 15:04:31 +0300

    added path-sum-iii.python

--- path-sum-iii.py
+++ path-sum-iii.py
@@ -1,21 +1,35 @@
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
-#         self.val = val 
+#         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        sums = defaultdict(int)
-        sums[0] = 1
-        def dfs(node,curr):
-            count = 0
-            if not node:
-                return 0
-            curr += node.val
-            count = sums[curr-targetSum] 
-            sums[curr] += 1
-            count += dfs(node.left,curr ) + dfs(node.right,curr)
-            sums[curr] -= 1
-            return count 
-        return dfs(root,0)
+        if not root: return 0
+        count = 0
+        def bfs(root):
+            nonlocal count
+            que = deque([(root,root.val)])
+            ans = []
+            while que:
+                node,curr_sum = que.popleft()
+                if curr_sum == targetSum: count += 1
+                if node.left:
+                    left = node.left
+                    left_sum = curr_sum + left.val
+                    que.append((left,left_sum))
+                if node.right:
+                    right = node.right
+                    right_sum = curr_sum + right.val
+                    que.append((right,right_sum))
+                # print(que)
+        def dfs(root):
+            if not root:
+                return 
+            else:
+                bfs(root)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return count
