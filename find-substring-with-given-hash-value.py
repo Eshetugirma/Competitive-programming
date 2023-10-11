f6eab278f0534c151cbae8f5b1ad4ca0e83a7e1b
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-10-11 18:10:42 +0300

    added find-substring-with-given-hash-value.python

--- find-substring-with-given-hash-value.py
+++ find-substring-with-given-hash-value.py
@@ -2,13 +2,14 @@ class Solution:
     def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
         window = 0
         const = power**(k)%modulo
-
+        # iterate backward so that we can ignore division by power which create problem
         for i in range(len(s)-1,-1,-1):
             window = (window*power + (ord(s[i])-96))%modulo
 
+            # if window is above k decrease 
             if i+k < len(s):
                 window = (window - (ord(s[i+k])-96)*const)%modulo
-            # window = window % modulo 
-            # print(window,hashValue)
+
+            # if we see valid window then update starting index 
             if window%modulo == hashValue: ind = i
         return s[ind:ind+k]
