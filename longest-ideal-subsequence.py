24966f1ed38b340bf8b520ddd6a78b667e2f58a6
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-10-17 15:52:30 +0300

    added longest-ideal-subsequence.python

---
+++ longest-ideal-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        n = len(s)
+        dp = [0]*(27+2*k)
+
+        for c in s:
+            index = ord(c) - 97 + k
+            dp[index] = max(dp[index-k:index+k+1])+1
+  
+        return max(dp)
