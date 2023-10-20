998faf678709c83e7cb971117864d64a37493610
Eshetugirma <115847211+Eshetugirma@users.noreply.github.com>
2023-10-20 22:13:53 +0300

    added divisible-and-non-divisible-sums-difference.python

---
+++ divisible-and-non-divisible-sums-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1,num2 = 0,0
+        for i in range(1,n+1):
+            if i%m:
+                num1 += i
+            else:
+                num2 += i
+        return num1-num2
