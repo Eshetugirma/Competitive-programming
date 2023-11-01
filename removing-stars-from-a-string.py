ef501c8b06dd982c56653f25c254d2c669ec37ea
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2023-11-01 23:47:23 +0300

    added removing-stars-from-a-string.python

---
+++ removing-stars-from-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i == "*":
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
