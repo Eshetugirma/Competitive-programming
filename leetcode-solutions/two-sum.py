084763d1e1c9731a8b26574fa8ce0c26276fd7bb
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2023-11-20 17:45:58 +0300

    Added solution for Two Sum

---
+++ leetcode-solutions/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sets = set(nums)
+        for i in range(len(nums)):
+            curr = target - nums[i] 
+            if curr != nums[i] and curr in sets:
+                # print(nums[i])
+                return [i,nums.index(curr)]
+            elif curr in sets:
+                temp = [i,nums.index(curr)]
+                
+        return temp
+        
