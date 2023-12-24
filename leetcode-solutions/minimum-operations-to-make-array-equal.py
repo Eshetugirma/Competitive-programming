171d4bbf25b2483540befabed5d1f0493c267349
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2023-12-25 00:14:07 +0300

    Added solution for Minimum Operations to Make Array Equal

--- leetcode-solutions/minimum-operations-to-make-array-equal.py
+++ leetcode-solutions/minimum-operations-to-make-array-equal.py
@@ -1,12 +1,4 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        stack = []
-        for i in range(n):
-            stack.append(2*i+1)
-        left, right,ans = 0, n-1,0
-        while left < right:
-            ans += (stack[left]+stack[right])//2 - stack[left]
-            left += 1
-            right -= 1
-        return ans            
+        return (n*n)//4   
         
