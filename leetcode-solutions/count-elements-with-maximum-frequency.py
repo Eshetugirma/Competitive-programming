9446a8e8aa67beb7394375c64f46560f24a65c2f
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-03-22 17:35:09 +0300

    Added solution for Count Elements With Maximum Frequency

--- leetcode-solutions/count-elements-with-maximum-frequency.py
+++ leetcode-solutions/count-elements-with-maximum-frequency.py
@@ -4,7 +4,7 @@ class Solution:
         mx = max(counter.values())
         ans = 0
         for key, val in counter.items():
-            print(counter,mx)
+            # print(counter,mx)
             if val == mx:
                 ans += val
         return ans
