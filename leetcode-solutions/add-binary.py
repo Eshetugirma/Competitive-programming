2c4c3e98bb5ab7b712463cc79ac1ba7c1f226fa0
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-06-20 23:05:12 +0300

    Added solution for Add Binary

---
+++ leetcode-solutions/add-binary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = '0'*(10**4-len(a)) + a
+        b = '0'*(10**4-len(b)) + b
+        answer = ['0']*10**4
+        carry = 0
+        
+        for i in range(10**4-1,-1,-1):
+            current = int(a[i]) + int(b[i])
+            if current == 2 and carry :
+                answer[i] ='1'
+            elif current == 2 and not carry or current == 1 and carry:
+                carry = 1
+            else:
+                answer[i] = str(current + carry)
+                carry = 0
+        # print(answer)
+        res = ''.join(answer)
+        res = res.lstrip('0')
+        return '0' if not res else res
