a85bfa5b25b97ab1aa1758d42c4eba8bf37c0cba
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-07-21 22:37:58 +0300

    Added solution for Delete Nodes And Return Forest

--- leetcode-solutions/delete-nodes-and-return-forest.py
+++ leetcode-solutions/delete-nodes-and-return-forest.py
@@ -2,29 +2,24 @@
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.left = left 
-#         self.right = right 
+#         self.left = left
+#         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-
-        toDelete = set(to_delete)
-        remainingForest = []
-
-        def deleteTree(root):
-            
+        to_delete = set(to_delete)
+        forest = []
+        def dfs(root):
             if root.left:
-                root.left = deleteTree(root.left)
+                root.left = dfs(root.left)
             if root.right:
-                root.right = deleteTree(root.right)
-            if root.val in toDelete:
+                root.right = dfs(root.right)
+            if root.val in to_delete:
                 if root.left:
-                    remainingForest.append(root.left)
+                    forest.append(root.left)
                 if root.right:
-                    remainingForest.append(root.right)
+                    forest.append(root.right)
                 return None
             return root
-
-        if deleteTree(root):
-            remainingForest.append(root)
-        return remainingForest
-
+        if dfs(root):
+            forest.append(root)
+        return forest
