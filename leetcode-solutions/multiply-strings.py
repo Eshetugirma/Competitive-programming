c37b0e509e37a561ae1c004651a00a20f68ff5e6
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-09-30 21:46:04 +0300

    Added solution for Multiply Strings

---
+++ leetcode-solutions/multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        number1=int(num1)
+        number2=int(num2)
+        n3=number1*number2
+        
+        n=str(n3)
+        return n
