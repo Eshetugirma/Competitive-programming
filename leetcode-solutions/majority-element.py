9fbd7c2da065274c9932e2c738e3e2dabaeaed7e
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-10-07 16:53:44 +0300

    Added solution for Majority Element

--- leetcode-solutions/majority-element.py
+++ leetcode-solutions/majority-element.py
@@ -1,8 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        n = len(nums)//2
-    
-        for key,val in count.items():
-            if val > n:
-                return key
+
+        current = 0
+        count = 0
+        for num in nums:
+            
+            if count == 0:
+                current = num
+                
+            count += current == num
+            count -= current != num
+
+        return current 
+
+            
+
