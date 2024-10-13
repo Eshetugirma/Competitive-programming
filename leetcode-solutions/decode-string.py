8c1d681ee8e42aaa9239384087063d5f3887fe72
Eshetu Girma <115847211+Eshetugirma@users.noreply.github.com>
2024-10-13 14:38:55 +0300

    Added solution for Decode String

--- leetcode-solutions/decode-string.py
+++ leetcode-solutions/decode-string.py
@@ -1,28 +1,29 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        dig = ['1','2','3','4','5','6','7','8','9','0']
+        
+        integers = ['1','2','3','4','5','6','7','8','9','0']
         stack = []
-        #==>>> edge case if no brackets
-        if "[" not in s and s[0] in dig:
-            return ""
-        elif "[" not in s and s[0] not in dig:
-            return s
-        #==>> using stack we can solve this problem
-        for i in range(len(s)):
-            #==>> append any element until u see "]"
-            if s[i] != "]":
-                stack.append(s[i])
-            #==>>> if see "]" then pop str after opening brace
-            else:
-                substr = ""
-                while stack[-1] != "[":
-                    substr = stack.pop() + substr
+
+        for char in s:
+
+            if char == ']':
+
+                temp = ''
+                while stack[-1] != '[':
+                    temp = stack.pop() + temp
+
                 stack.pop()
-                num = ""
-                #==>> if see digit collect on num 
-                while stack and stack[-1] in dig:
+
+                num = ''
+                while stack and stack[-1] in integers:
                     num = stack.pop() + num
-                #==>> now multiply substr by num and append back to stack 
-                stack.append(int(num) * substr)
-        return "".join(stack)
-                    
+                
+                try:
+                    char = temp*(int(num))
+
+                except:
+                    char = temp
+            
+            stack.append(char)
+
+        return ''.join(stack)
